f7b1f41b1575401b7d1318b65d66be5164f363da
Vasanta Ghetman <112274976+Vasanta2002@users.noreply.github.com>
2024-04-21 20:18:11 -0400

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,25 +1,31 @@
 """Search an array for a target in log n time.
 The list must be sorted in order of smallest to biggest."""
-
-List = [-9, -4, -2, 1, 5, 8, 9]
-target = 5
-
 def search(nums: [], target: int):
-    l = 0
-    r = len(nums)-1
+    """
+    Search for a target in a sorted array using a binary search algorithm.
 
+    Parameters:
+        nums (list): A sorted list of integers.
+        target (int): The integer to search for in the list.
+
+    Returns:
+        int: The index of the target if found, otherwise -1.
+    """
+    l = 0
+    r = len(nums) - 1
 
     while l <= r:
-        m = (l+r)//2
+        m = (l + r) // 2
 
         if target == nums[m]:
             return m
         elif target < nums[m]:
-            r = m-1
+            r = m - 1
         elif target > nums[m]:
-            l = m+1
+            l = m + 1
 
-    return -1 # Time: O(log(n)), Space: 0(1)
+    return -1  # Time: O(log(n)), Space: O(1)
 
+List = [-9, -4, -2, 1, 5, 8, 9]
+target = 5
 print(search(List, target))
-
