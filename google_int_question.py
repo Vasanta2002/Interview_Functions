1fa1b78434cd0f03a7e471020a88ce1e127c3850
Vasanta Ghetman <112274976+Vasanta2002@users.noreply.github.com>
2024-04-21 20:18:58 -0400

    Update google_int_question.py

--- google_int_question.py
+++ google_int_question.py
@@ -1,17 +1,25 @@
 """Unique Paths
 
-there is a robot on an 'm' x 'n' grid. The robot is initially lacated ar the top-left corner.
+There is a robot on an 'm' x 'n' grid. The robot is initially located at the top-left corner.
 The robot tries to move to the bottom-right corner. The robot can only move either down or
- right at any point in time"""
+right at any point in time.
 
+Given the two integers m and n, return the number of possible unique paths that the robot 
+can take to reach the bottom-right corner.
+"""
 
-""""Given the two integers m and n, return the number of possible 
-unique paths that the robot can take to reach the bottom-right corner."""
+def uniquePaths(self, m: int, n: int) -> int:
+    """
+    Calculate the number of unique paths for a robot to move from the top-left corner to
+    the bottom-right corner of an 'm' x 'n' grid.
 
+    Parameters:
+        m (int): The number of rows in the grid.
+        n (int): The number of columns in the grid.
 
-
-
-def uniquePaths(self, m: int, n: int) ->int:
+    Returns:
+        int: The number of possible unique paths.
+    """
     
     # 1. Initialize Board of Zeros
     ret = []
@@ -27,9 +35,9 @@ def uniquePaths(self, m: int, n: int) ->int:
             if i == 0 and j == 0:
                 continue
             elif i == 0:
-                ret[i][j] = ret[i][j-1]
+                ret[i][j] = ret[i][j - 1]
             else:
-                ret[i][j] = ret[i][j-1] + ret[i-1][j]
+                ret[i][j] = ret[i][j - 1] + ret[i - 1][j]
 
-    # 4. Return the Answere
+    # 4. Return the Answer
     return ret[-1][-1]
